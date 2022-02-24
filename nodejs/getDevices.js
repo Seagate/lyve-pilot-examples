7610046d89c863cb6e6c55336f2e48b54902b00b
Dieter Schnabel <dieter.schnabel@seagate.com>
2022-02-24 09:35:24 -0700

    Updates to license and gitignore

--- nodejs/getDevices.js
+++ nodejs/getDevices.js
@@ -1,3 +1,23 @@
+// Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to 
+// deal in the Software without restriction, including without limitation the 
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+// sell copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in 
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
+
 const axios = require('axios');
 const { exit } = require('process');
 
@@ -84,6 +104,9 @@ async function start() {
     let destVolumes = await getVolumes(jwt, { 'category': 'UDX' });
     console.log(JSON.stringify(destVolumes, null, 2));
 
+    // Select the first volume found on the device. This is
+    // where the code needs to be adjusted to select the desired volumes
+    // for source and destination.
     sourceUri = sourceVolumes['data'][0]['uri'];
     destUri = destVolumes['data'][0]['uri']
     destDeviceId = destVolumes['data'][0]['deviceId']
